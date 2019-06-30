c9bede37fdc4bea873e73d42a802e5ec5bc3a59f
pawprabhu17 <pawprabhu17@gmail.com>
2019-06-30 12:44:07 +0530

    Updated About Page

--- js/about.js
+++ js/about.js
@@ -1,43 +1,63 @@
-
-    $("li:nth-child(1)").hover(function(){
-        $(this).append($("<div class='html'>80%</div>").addClass("html"));
+$("li:nth-child(1)").hover(function(){
+    $(this).append($("<div class='html'>80%</div>").addClass("html"));    
     },function(){
         $(this).find("div:last").remove();
     }
-    );
+);
 
-    $("li:nth-child(2)").hover(function(){
-        $(this).append($("<div class='bootstrap'>75%</div>").addClass("bootstrap"));
+$("li:nth-child(2)").hover(function(){
+    $(this).append($("<div class='bootstrap'>75%</div>").addClass("bootstrap"));
     },function(){
         $(this).find("div:last").remove();
     }
-    );
+);
 
-    $("li:nth-child(3)").hover(function(){
-        $(this).append($("<div class='css'>70%</div>").addClass("css"));
+$("li:nth-child(3)").hover(function(){
+    $(this).append($("<div class='css'>70%</div>").addClass("css"))
     },function(){
         $(this).find("div:last").remove();
     }
-    );
+);
 
-    $("li:nth-child(4)").hover(function(){
-        $(this).append($("<div class='java'>60%</div>").addClass("java"));
+$("li:nth-child(4)").hover(function(){
+    $(this).append($("<div class='java'>60%</div>").addClass("java"));
     },function() {
         $( this ).find( "div:last" ).remove();
     }
-    );
+);
 
-    $("li:nth-child(5)").hover(function(){
-        $(this).append($("<div class='javascript'>50%</div>").addClass("javascript"));
+$("li:nth-child(5)").hover(function(){
+    $(this).append($("<div class='javascript'>50%</div>").addClass("javascript"));
     },function(){
         $(this).find("div:last").remove();
     }
-    );   
+);   
 
-    $("li:nth-child(6)").hover(function(){
-        $(this).append($("<div class='angular'>30%</div>").addClass("angular"));
+$("li:nth-child(6)").hover(function(){
+    $(this).append($("<div class='angular'>30%</div>").addClass("angular"));
     },function(){
         $(this).find("div:last").remove();
     }
-    );
+);
 
+
+$("ul div:nth-child(1)").hover(function(){
+    $(this).append($("<div class='marks'>CGPA: 8.72</div>").addClass("marks"));
+    },function(){
+    $(this).find("div:last").remove();
+    }
+);
+
+$("ul div:nth-child(2)").hover(function(){
+    $(this).append($("<div class='marks'>PERCENTAGE: 93.66%</div>").addClass("marks"));
+    },function(){
+    $(this).find("div:last").remove();
+    }
+);
+
+$("ul div:nth-child(3)").hover(function(){
+    $(this).append($("<div class='marks'>CGPA: 10</div>").addClass("marks"));
+    },function(){
+    $(this).find("div:last").remove();
+    }
+);
